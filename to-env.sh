9b1981f999ae57af308f299154199b686665e54b
Von Welch <von@vwelch.com>
2013-09-20 18:24:12 -0400

    to-env.sh: whitespace clean up.

--- to-env.sh
+++ to-env.sh
@@ -12,31 +12,34 @@
 
 function to-env()
 {
-    local prefix="e"
-    local usage="Usage: to-env [-p <prefix>] <command>"
-    local OPTIND  # Not just a good idea, required for proper bahavior
-    while getopts ":p:" opt "$@" ; do
-	case $opt in
-	    p) prefix=${OPTARG} ;;
-	    \?) echo "Invalid option: -$OPTARG" >&2 ;;
-	esac
-    done
-    shift $(($OPTIND - 1))
-    if test $# -eq 0 ; then
-	echo "Missing command."
-	echo $usage
-	return 1
-    fi
-    local output="$(eval $@)"
-    if test -z "${output}" ; then
-	echo "No output."
-	return 0
-    fi
-    local count=1
-    while read -r line ; do
-	local var=${prefix}${count}
-	eval "${var}='${line}'"
-	echo "\$${var}: ${line}"
-	let count=count+1
-    done <<< "$output"
+  local prefix="e"
+  local usage="Usage: to-env [-p <prefix>] <command>"
+  local OPTIND  # Not just a good idea, required for proper bahavior
+  while getopts ":p:" opt "$@" ; do
+    case $opt in
+      p) prefix=${OPTARG} ;;
+      \?) echo "Invalid option: -$OPTARG" >&2 ;;
+    esac
+  done
+
+  shift $(($OPTIND - 1))
+  if test $# -eq 0 ; then
+    echo "Missing command."
+    echo $usage
+    return 1
+  fi
+
+  local output="$(eval $@)"
+  if test -z "${output}" ; then
+    echo "No output."
+    return 0
+  fi
+
+  local count=1
+  while read -r line ; do
+    local var=${prefix}${count}
+    eval "${var}='${line}'"
+    echo "\$${var}: ${line}"
+    let count=count+1
+  done <<< "$output"
 }
